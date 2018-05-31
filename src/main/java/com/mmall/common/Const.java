95dfb6d7f1ad8698a5887292c2955f0425dd8e86
redli <redlixh001@gmail.com>
2018-05-31 21:34:03 +0800

    异常处理

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -18,6 +18,9 @@ public class Const {
     public static final String TOKEN_PREFIX = "token_";
 
     public interface RedisCacheExTime {
+        /**
+         * 30分钟
+         */
         int REDIS_SESSION_EXTIME = 60 * 30;
     }
 
@@ -172,5 +175,11 @@ public class Const {
             }
             throw new RuntimeException("没有找到对应的枚举");
         }
+    }
+    public interface  REDIS_LOCK{
+        /**
+         * 关闭订单的分布式锁
+         */
+        String CLOSE_ORDER_TASK_LOCK = "CLOSE_ORDER_TASK_LOCK";
     }
 }
--- src/main/java/com/mmall/pojo/User.java
+++ src/main/java/com/mmall/pojo/User.java
@@ -5,13 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * @author redli
+ */
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements Serializable {
     private Integer id;
 
     private String username;
